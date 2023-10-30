0249d57d9c4950eb211866654f2852ec9686005b
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-30 19:30:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,2 @@
-//your JS code here. If required.
-document.getElementById("change_button").addEventListener("click", function () {
-    const blockId = document.getElementById("block_id").value;
-    const color = document.getElementById("colour_id").value;
-    
-    const gridItem = document.getElementById(blockId);
 
-    if (gridItem) {
-        // Change the background color of the selected grid item
-        gridItem.style.backgroundColor = color;
-    } else {
-        alert("Invalid block ID. Please enter a valid ID.");
-    }
-});
-
-document.getElementById("reset_button").addEventListener("click", function () {
-    // Reset all grid items to transparent background
-    const gridItems = document.getElementsByClassName("grid-item");
-    for (let i = 0; i < gridItems.length; i++) {
-        gridItems[i].style.backgroundColor = "transparent";
-    }
-});
 
