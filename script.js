ba324eab0de9bd98e1c8aa157a47c087f377b0a1
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2024-01-10 18:28:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-
+document.getElementById('change_button').addEventListener('click', function() {
+    const blockId = document.getElementById('block_id').value;
+    const color = document.getElementById('colour_id').value;
+    const gridItem = document.getElementById(blockId);
+  
+    if (!gridItem) {
+      alert('Invalid block ID');
+      return;
+    }
+  
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => {
+      item.style.backgroundColor = 'transparent';
+    });
+  if(color)
+  {
+    gridItem.style.backgroundColor = color;
+  }
+    
+  });
+  
+  document.getElementById('reset_button').addEventListener('click', function() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => {
+      item.style.backgroundColor = 'transparent';
+    });
+  });
 
